73d20b6f965e997f21cae18e17cc0d538cb10fab
Sergio Chan <yuheng9211@qq.com>
2016-08-25 14:43:01 +0800

    Site updated: 2016-08-25 14:43:00

--- js/main.js
+++ js/main.js
@@ -28,24 +28,28 @@ $(document).ready(function() {
 
   $('.btn-mobile-menu__icon').click(function() {
     if ($('.navigation-wrapper').css('display') == "block") {
-      $('.navigation-wrapper').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
-        $('.navigation-wrapper').toggleClass('visible animated bounceOutUp');
-      });
-      $('.navigation-wrapper').toggleClass('animated bounceInDown animated bounceOutUp');
-
+      // $('.navigation-wrapper').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
+      //   $('.navigation-wrapper').toggleClass('visible animated bounceOutUp');
+      // });
+      // $('.navigation-wrapper').toggleClass('animated bounceInDown animated bounceOutUp');
+      $('.navigation-wrapper').css('display',"none");
     } else {
-      $('.navigation-wrapper').toggleClass('visible animated bounceInDown');
+      $('.navigation-wrapper').css('display',"block");
+      // $('.navigation-wrapper').toggleClass('visible animated bounceInDown');
     }
     $('.btn-mobile-menu__icon').toggleClass('fa fa-list fa fa-angle-up animated fadeIn');
   });
 
   $('.navigation-wrapper .blog-button').click(function() {
     if ($('.navigation-wrapper').css('display') == "block") {
-      $('.navigation-wrapper').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
-        $('.navigation-wrapper').toggleClass('visible animated bounceOutUp');
-      });
+      $('.navigation-wrapper').css('display',"none");
+      // $('.navigation-wrapper').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
+      //   $('.navigation-wrapper').toggleClass('visible animated bounceOutUp');
+      // });
 
-      $('.navigation-wrapper').toggleClass('animated bounceInDown animated bounceOutUp');
+      // $('.navigation-wrapper').toggleClass('animated bounceInDown animated bounceOutUp');
+    } else {
+      $('.navigation-wrapper').css('display',"block");
     }
     $('.btn-mobile-menu__icon').toggleClass('fa fa-list fa fa-angle-up animated fadeIn');
   });
